094a76c577725891657e7a5bf368b68ce3df66c2
emanawad14 <emanelnady2001@gmail.com>
2025-05-17 18:00:15 +0300

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// config.js
+window.API_CONFIG = {
+    BASE_URL: 'https://lokta.sy/api', 
+    ENDPOINTS: {
+        LOGIN: '/api/login', 
+    },
+    DEFAULT_HEADERS: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+};
